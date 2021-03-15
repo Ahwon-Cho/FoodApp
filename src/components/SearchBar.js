5b163e13653402dcaa018026acd9091ef1dca8bb
Ahwon Cho <awony82@gmail.com>
2021-03-15 16:34:03 -0700

    added SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,19 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 const SearchBar = () => {
-    return <View>
+    return (
+        <View style={stayles.background}>
         <Text>SearchBar</Text>
     </View>
+    )
 
     
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    bakcground: {
+        backgroundColor: '#f0eeee'
+    }
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import SearchBar from ../compoenets/SearchBar''
 
 const SearchScreen = () => {
     return <View>
