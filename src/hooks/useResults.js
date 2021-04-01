32f47d1ff0fb4e516274539235c54323b0cee071
Ahwon Cho <awony82@gmail.com>
2021-04-01 16:08:11 -0700

    created hook

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -3,5 +3,29 @@ import yelp from '../api/yelp';
 
 
 export default () => {
+    const [ results, setResults ] = useState ([]);
+    const [ errorMessage, setErrorMessage] = useState ('')
 
+    const searchApi = async (searchTerm) => {
+        try {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term : searchTerm,
+                location: 'seattle'
+            }
+        });
+        setResults(response.data.businesses)
+    } catch (err) {
+        setErrorMessage ('seomthign went wrong')
+    }
+    };
+
+// call searchAPi when compoenet
+// is first render
+userEffect(() => {
+    searchApi('pasta');
+}, []);
+
+return [searchApi, results, errorMessage];
 };
